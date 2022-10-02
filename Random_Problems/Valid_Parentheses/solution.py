96eb71dc125021a393009916519a67b6aa47ceea
Gtindi <tindigabriel@gmail.com>
2022-10-03 00:52:14 +0300

    Valid Parentheses Solution

---
+++ Random_Problems/Valid_Parentheses/solution.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+        closeToOpen = { ")" : "(", "]" : "[", "}" : "{" }
+
+        for c in s:
+            if c in closeToOpen:
+                if stack and stack[-1] == closeToOpen[c]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(c)
+
+        return True if not stack else False
+
